ba9d6e68bcde977ef2a3be9aa6bb0d7a5240cbf7
jpelaa <jpelaa1991994@gmail.com>
2023-11-11 09:47:10 +0530

    basic form setup

---
+++ src/static/products.js
@@ -0,0 +1,144 @@
+export const PRODUCT_FORM_FIELDS = {
+  title: {
+    keyName: "title",
+    labelName: "Title",
+  },
+  price: {
+    keyName: "price",
+    labelName: "Price",
+  },
+  currency: {
+    keyName: "currency",
+    labelName: "Currency",
+  },
+  color: {
+    keyName: "color",
+    labelName: "Color",
+  },
+  gender: {
+    keyName: "gender",
+    labelName: "Gender",
+  },
+  ageGroup: {
+    keyName: "ageGroup",
+    labelName: "Age Group",
+  },
+  groupCode: {
+    keyName: "groupCode",
+    labelName: "Group Code",
+  },
+  size: {
+    keyName: "size",
+    labelName: "Sizes",
+  },
+  availableSizes: {
+    keyName: "availableSizes",
+    labelName: "Available Sizes",
+  },
+  stockQuantity: {
+    keyName: "stockQuantity",
+    labelName: "Stock Quantity",
+  },
+  description: {
+    keyName: "description",
+    labelName: "Description",
+  },
+  images: {
+    keyName: "images",
+    labelName: "Images",
+  },
+  category: {
+    keyName: "category",
+    labelName: "category",
+  },
+  categoryId: {
+    keyName: "categoryId",
+    labelName: "categoryId",
+  },
+  marketedBy: {
+    keyName: "marketedBy",
+    labelName: "marketedBy",
+  },
+  countryOfOrigin: {
+    keyName: "countryOfOrigin",
+    labelName: "countryOfOrigin",
+  },
+  brand: {
+    keyName: "brand",
+    labelName: "Brand Name",
+  },
+  productCode: {
+    keyName: "productCode",
+    labelName: "Product Code",
+  },
+  mrp: {
+    keyName: "mrp",
+    labelName: "MRP",
+  },
+  seller: {
+    keyName: "seller",
+    labelName: "Seller",
+  },
+  occasion: {
+    keyName: "occasion",
+    labelName: "occasion",
+  },
+  sleeveLength: {
+    keyName: "sleeveLength",
+    labelName: "Sleeve Length",
+  },
+  pattern: {
+    keyName: "pattern",
+    labelName: "Pattern",
+  },
+  patternType: {
+    keyName: "patternType",
+    labelName: "patternType",
+  },
+  washCare: {
+    keyName: "washCare",
+    labelName: "washCare",
+  },
+  weavePattern: {
+    keyName: "weavePattern",
+    labelName: "weavePattern",
+  },
+  searchPossibles: {
+    keyName: "searchPossibles",
+    labelName: "searchPossibles",
+  },
+  noOfPockets: {
+    keyName: "noOfPockets",
+    labelName: "noOfPockets",
+  },
+  mainFabricType: {
+    keyName: "mainFabricType",
+    labelName: "mainFabricType",
+  },
+  secondaryFabricType: {
+    keyName: "secondaryFabricType",
+    labelName: "secondaryFabricType",
+  },
+  material: {
+    keyName: "material",
+    labelName: "material",
+  },
+  collarType: {
+    keyName: "collarType",
+    labelName: "collarType",
+  },
+  length: {
+    keyName: "length",
+    labelName: "length",
+  },
+  fit: {
+    keyName: "fit",
+    labelName: "fit",
+  },
+};
+
+export const PRODUCT_FORM_ORDER = [
+  PRODUCT_FORM_FIELDS.title.keyName,
+  PRODUCT_FORM_FIELDS.title.price,
+  PRODUCT_FORM_FIELDS.title.currency,
+];
